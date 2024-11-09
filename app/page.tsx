c914957c21be67249acfe3d90429a65f2f27f580
m483732 <marcelo.dc.santana@bradesco.com.br>
2024-11-08 23:19:55 -0300

    core: add tailwind plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
 const Home = () => {
-  return <h1>Hello</h1>;
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="text-red-500">Hello</h1>
+    </div>
+  );
 };
 export default Home;
