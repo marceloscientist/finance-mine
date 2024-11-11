702c1d0c9199e2c88a3c7d09a3d3ac33e33df1d5
m483732 <marcelo.dc.santana@bradesco.com.br>
2024-11-11 08:26:03 -0300

    chore: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
     <div className="flex w-screen items-center justify-center">
       <h1 className="text-red-500">Hello</h1>
+      <Button>Hello World</Button>
     </div>
   );
 };
